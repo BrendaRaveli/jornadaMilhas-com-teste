b1a4d64fca6378739317630dd53ef881f24ca90f
BrendaRaveli <brendaraveli00@gmail.com>
2024-06-13 06:47:33 -0300

    feat: validade da data de partida da viagem

--- tests/JornadaMilhas.Teste/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Teste/OfertaViagemTest.cs
@@ -8,7 +8,7 @@ namespace JornadaMilhas.Teste
         public void TestandoOfertaValida()
         {
             Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-            Periodo periodo = new Periodo (new DateTime(2024, 02 ,1), new DateTime(2024,2,5));
+            Periodo periodo = new Periodo(new DateTime(2024, 02, 1), new DateTime(2024, 2, 5));
             double preco = 100.0;
             var validacao = true;
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
